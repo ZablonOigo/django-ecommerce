0a064252378b445646de0c00975e18d51073b90c
Zablon Oigo <imjunior254@gmail.com>
2023-03-28 09:34:39 +0300

    login redirect url

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,3 +125,7 @@ MEDIA_ROOT=BASE_DIR/ 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL='login'
+LOGIN_REDIRECT_URL='store:index'
+LOGOUT_REDIRECT_URL='/'
